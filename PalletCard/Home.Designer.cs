a4c0ca14a7fee4519c9676d18510c8c3b60b9bb7
declan enright <decenright@gmail.com>
2017-05-31 10:13:33 +0100

    odbc connection to Tharstern with sql query to fill datagridview

---
+++ PalletCard/Home.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace PalletCard
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1012, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 197);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1109, 440);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Home";
+            this.Text = "Pallet Card App";
+            this.Load += new System.EventHandler(this.Home_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PalletCard/Home.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.Odbc;
+using System.Configuration;
+using System.Data;
+using System.Collections.Generic;
+
+namespace PalletCard
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+            //OdbcConnection conn = new OdbcConnection();
+            //conn.ConnectionString = "Dsn=TharData;uid=tharuser";
+            //try
+            //{
+            //    conn.Open();
+            //    using (OdbcCommand com = new OdbcCommand("SELECT JobNo FROM app_PalletOperations", conn))
+            //    {
+            //        using (OdbcDataReader reader = com.ExecuteReader())
+            //        {
+            //            while (reader.Read())
+            //            {
+            //                string word = reader.GetString(0);
+            //                // Word is from the database. Do something with it.
+            //                label1.Text = reader.GetValue(0).ToString();
+            //            }
+            //        }
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show("Failed to connect to data source");
+            //}
+            //finally
+            //{
+            //    conn.Close();
+            //}
+
+
+            string ConnectionString = Convert.ToString("Dsn=TharData;uid=tharuser");
+            string CommandText = "SELECT * FROM app_PalletOperations";
+
+            OdbcConnection myConnection = new OdbcConnection(ConnectionString);
+            OdbcCommand myCommand = new OdbcCommand(CommandText, myConnection);
+
+            OdbcDataAdapter myAdapter = new OdbcDataAdapter();
+            myAdapter.SelectCommand = myCommand;
+            DataSet myDataSet = new DataSet();
+            try
+            {
+                myConnection.Open();
+                myAdapter.Fill(myDataSet);
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+
+            using (DataTable dt = new DataTable())
+            {
+                myAdapter.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+        }
+    }
+}
